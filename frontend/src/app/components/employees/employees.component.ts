bc2cfec41f88519670f9238815f992e8e7c0cde2
Ignacio <shoik.xtazy@gmail.com>
2019-07-16 01:24:32 -0400

    adding module 'cors' and save new employee :D

--- frontend/src/app/components/employees/employees.component.ts
+++ frontend/src/app/components/employees/employees.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { EmployeeService } from '../../services/employee.service';
+import { NgForm } from '@angular/forms';
+import { Employee } from 'src/app/models/employee';
 
 @Component({
   selector: 'app-employees',
@@ -13,6 +15,21 @@ export class EmployeesComponent implements OnInit {
   constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
+  }
+
+  addEmployee(form: NgForm){
+    //console.log(form.value);
+    this.employeeService.postEmployee(form.value)
+      .subscribe(res => {
+        console.log(res);
+      });
+  }
+
+  resetForm(form? :NgForm){
+    if(form){
+      form.reset();
+      this.employeeService.selectedEmployee = new Employee();
+    }
   }
 
 }
--- server/test.js
+++
@@ -1,7 +0,0 @@
-myfunc = () => {
-    let a = [1,2,3]; 
-}; 
-
-myfunc();
-
-console.log(a);
